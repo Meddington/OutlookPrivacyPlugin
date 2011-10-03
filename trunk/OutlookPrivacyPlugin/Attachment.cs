9ed66c0edcb4eca34e59d8682051087d024663f8
mike@dejavusecurity.com <mike@dejavusecurity.com@11eac574-72d0-83a2-a144-0779a0940029>
2011-10-03 23:43:48 +0000

    Attachments are now supported
    git-svn-id: http://outlook-privacy-plugin.googlecode.com/svn@7 11eac574-72d0-83a2-a144-0779a0940029

---
+++ trunk/OutlookPrivacyPlugin/Attachment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutlookPrivacyPlugin
+{
+	public class Attachment
+	{
+		public string FileName { get; set; }
+		public string DisplayName { get; set; }
+		public object AttachmentType { get; set; }
+		public string TempFile { get; set; }
+		public bool Encrypted { get; set; }
+	}
+}
